2dcd0baa59927d2d626ef92e96041f272ac959f9
Tessa Rivas <rivas.gomez.teresa@gmail.com>
2025-03-13 12:26:20 -0700

    cambiosssssss

--- src/pages/RegisterIdea.jsx
+++ src/pages/RegisterIdea.jsx
@@ -2,7 +2,7 @@ export default function RegisterIdea() {
     return (
         <>
         <div className="p-10 text-4xl text-primary font-bold">
-            <h1>Legisladores</h1>
+            <h1>Registrar Idea</h1>
         </div>
         </>
     );
