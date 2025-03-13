66978f66483f2e28adf041739361bdab92ce3755
Tessa Rivas <rivas.gomez.teresa@gmail.com>
2025-03-13 13:04:41 -0700

    hola ya esta la ruta y no se que mas

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import Home from './pages/home.jsx';
 import Proposals from './pages/AllProposals.jsx';
+import SelectedProposal from './pages/SelectedProposal.jsx';
 import RegisterIdeas from './pages/IdeaRegister.jsx';
 import Register from './pages/Register.jsx';
 import Login from './pages/Login.jsx';
@@ -13,11 +14,12 @@ const root = document.getElementById("root");
 
 createRoot(root).render(
     <StrictMode>
-        <BrowserRouter basename="/NotDefined">
+        <BrowserRouter basename="/NotDefined/"> 
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route index element={<Home />} />
                     <Route path="proposals" element={<Proposals />} />
+                    <Route path="selectedProposal" element={<SelectedProposal />} />
                     <Route path="registerIdeas" element={<RegisterIdeas />} />
                     <Route path="register" element={<Register />} />
                     <Route path="login" element={<Login />} />
---
+++ src/pages/SelectedProposal.jsx
@@ -0,0 +1,9 @@
+export default function SelectedProposal() {
+  return (
+    <>
+      <div>
+        <h1>Propuesta Seleccionada</h1>
+      </div>
+    </>
+  );
+}
