96ac3c32290db5338e169e0d07c4a948b369a5fc
Tessa Rivas <rivas.gomez.teresa@gmail.com>
2025-03-13 12:24:12 -0700

    cambios

--- src/pages/Proposals.jsx
+++ src/pages/Proposals.jsx
@@ -1,8 +1,8 @@
-export default function Iniciatives() {
+export default function Proposals() {
     return (
         <>
         <div className="p-10 text-5xl text-primary font-bold">
-            <h1>Iniciativas</h1>
+            <h1>Propuestas</h1>
         </div>
         </>
     );
--- src/pages/RegisterIdea.jsx
+++ src/pages/RegisterIdea.jsx
@@ -1,4 +1,4 @@
-export default function Legislators() {
+export default function RegisterIdea() {
     return (
         <>
         <div className="p-10 text-4xl text-primary font-bold">
